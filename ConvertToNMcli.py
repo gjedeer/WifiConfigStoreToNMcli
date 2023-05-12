4bd382ae775cac18969bfa73b3c8f84944c7e078
GDR! <gdr@gdr.name>
2023-05-12 18:34:05 +0200

    Bind connections to a wi-fi interface

--- ConvertToNMcli.py
+++ ConvertToNMcli.py
@@ -3,14 +3,15 @@
 import datetime
 import os
 import pipes
+import re
+import subprocess
 import sys
 import xml.etree.ElementTree as ET
 
-def emitCommand(ssid, password, encryption, hidden=False, device=None, outfile=None):
-    if encryption == "nopass":
-        command_string = "nmcli d wifi connect %s" % (pipes.quote(ssid),)
-    elif encryption == "WPA":
-        command_string = "nmcli d wifi connect %s password %s" % (pipes.quote(ssid), pipes.quote(password))
+def emitCommand(ssid, password, encryption, hidden=False, outfile=None):
+    command_string = "nmcli c add type wifi con-name %s ssid %s ifname $DEVICE" % (pipes.quote(ssid),pipes.quote(ssid),)
+    if encryption == "WPA":
+        command_string += "\nnmcli c modify %s wifi-sec.key-mgmt wpa-psk wifi-sec.psk %s" % (pipes.quote(ssid), pipes.quote(password))
 
     outfile.write('%s\n' % command_string)
     
@@ -55,13 +56,32 @@ def extractNetwork(file_path):
         
     return networks
 
+def guess_device():
+    sub = subprocess.run(['nmcli', '-f', 'DEVICE,TYPE', 'dev'], capture_output=True)
+    for line in sub.stdout.decode('utf8').split('\n'):
+        parts = re.split(r'\s+', line)
+        device = parts[0].strip()
+        type = parts[1].strip()
+        if type == 'wifi':
+            return device
+
+    print('Could not find a wifi device on this system, using wlan0')
+    return 'wlan0'
+
+
+
 def main():
     # Parse XML file
     networks = extractNetwork(sys.argv[1])
+    if len(sys.argv) > 2:
+        device = sys.argv[2]
+    else:
+        device = guess_device()
     
     with open('nmcli-wifi.sh', 'w') as f:
         f.write('#!/bin/sh\n')
         f.write('# Wi-Fi configuration file created from %s by ConvertToNMCli.py at %s\n\n' % (sys.argv[1], datetime.datetime.now().isoformat()))
+        f.write('DEVICE=%s\n\n' % pipes.quote(device))
         for wifi in networks:
             # SSID, PASSWORD, ENCRYPTION
             emitCommand(wifi[0], wifi[1], wifi[2], outfile=f)
@@ -70,6 +90,6 @@ def main():
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print('Usage: %s /path/to/WifiConfigStore.xml' % sys.argv[0])
+        print('Usage: %s /path/to/WifiConfigStore.xml <wifi_device_name>' % sys.argv[0])
         sys.exit(1)
     main()
